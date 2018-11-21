cab0e7cf80f9a554a7708e3692bd8d55e8610ad4
jdalzatec <jdalzatec@unal.edu.co>
2018-11-21 18:01:50 -0500

    Making example2

--- src/main.py
+++ src/main.py
@@ -1,25 +1,23 @@
 from scipy.integrate import simps
 from matplotlib import pyplot
+import matplotlib
+matplotlib.style.use('classic')
 import numpy
 
 pi = numpy.pi
 
-def K(kto2, points=100):
+def K(kto2, points=1000):
     phi = numpy.linspace(0, 0.5 * pi, points)
     values = numpy.zeros_like(kto2)
-    for i in range(len(kto2)):
-        func = 1.0 / numpy.sqrt(1 - kto2[i] * numpy.sin(phi))
-        values[i] = simps(func, phi)
-    return values
+    func = 1.0 / numpy.sqrt(1 - kto2 * numpy.sin(phi))
+    return simps(func, phi)
 
 
-def E(kto2, points=100):
+def E(kto2, points=1000):
     phi = numpy.linspace(0, 0.5 * pi, points)
     values = numpy.zeros_like(kto2)
-    for i in range(len(kto2)):
-        func = numpy.sqrt(1 - kto2[i] * numpy.sin(phi))
-        values[i] = simps(func, phi)
-    return values
+    func = numpy.sqrt(1 - kto2 * numpy.sin(phi))
+    return simps(func, phi)
 
 
 def Br(r, z, d=1.0, mu0=1.0, N=10, I=1.0, R=1.0):
@@ -46,28 +44,32 @@ def Bz(r, z, d=1.0, mu0=1.0, N=10, I=1.0, R=1.0):
            (((mu0 / (2.0 * pi)) * N * I) / numpy.sqrt((R + r)**2 + (z - 0.5 * d)**2)) * \
            ((R**2 - r**2 - (z - 0.5 * d)**2) * E2 / ((R - r)**2 + (z - 0.5 * d)**2) + K2)
 
-def main():
-    z = numpy.linspace(-1, 1, 50)
 
-
+def example1():
+    z_arr = numpy.linspace(-1, 1, 50)
     mu0=1.0
     N=100
     I=1.0
     R=1.0
     d = R
-    r = 0.00001
+    r_arr = [0.00001]
 
-    Bz_arr = Bz(r, z, d=d, mu0=mu0, N=N, I=I, R=R)
-    Br_arr = Br(r, z, d=d, mu0=mu0, N=N, I=I, R=R)
+    Bz_arr = numpy.zeros((len(z_arr), len(r_arr)))
+    Br_arr = numpy.zeros((len(z_arr), len(r_arr)))
+
+    for i, z in enumerate(z_arr):
+        for j, r in enumerate(r_arr):
+            Bz_arr[i, j] = Bz(r, z, d=d, mu0=mu0, N=N, I=I, R=R)
+            Br_arr[i, j] = Br(r, z, d=d, mu0=mu0, N=N, I=I, R=R)
 
     Bz_theo = 0.5 * mu0 * N * I * R**2 * (
-        (R**2 + (z + 0.5 * d)**2)**(-3/2) + \
-        (R**2 + (z - 0.5 * d)**2)**(-3/2)
+        (R**2 + (z_arr + 0.5 * d)**2)**(-3/2) + \
+        (R**2 + (z_arr - 0.5 * d)**2)**(-3/2)
         )
 
     pyplot.figure()
-    pyplot.plot(z, Bz_arr, "o", label=r"$B_{z}^{\rm num.}$")
-    pyplot.plot(z, Bz_theo, "--k", lw=2, label=r"$B_{z}^{\rm theo.}$")
+    pyplot.plot(z_arr, Bz_arr[:, 0], "o", label=r"$B_{z}^{\rm num.}$")
+    pyplot.plot(z_arr, Bz_theo, "--k", lw=2, label=r"$B_{z}^{\rm theo.}$")
     pyplot.axvline(d*0.5, lw=2, ls="-", color="crimson")
     pyplot.axvline(-d*0.5, lw=2, ls="-", color="crimson")
     pyplot.axhline(8*mu0*N*I / (numpy.sqrt(125) * R), lw=2, ls="--", color="royalblue")
@@ -78,6 +80,57 @@ def main():
     pyplot.tight_layout()
     pyplot.savefig("Bz_vs_z.pdf")
     pyplot.close()
+
+
+def example2():
+    mu0=1.0
+    N=100
+    I=4.8
+    R=0.475
+    d = R
+    z_arr = numpy.linspace(0, R/3, 50)
+    r_arr = numpy.linspace(0.000001, R, 50)
+    
+    z_grid, r_grid = numpy.meshgrid(z_arr, r_arr)
+    z_grid = z_grid.T
+    r_grid = r_grid.T
+
+    Bz_grid = numpy.zeros_like(z_grid)
+    Br_grid = numpy.zeros_like(z_grid)
+
+    for i in range(len(z_arr)):
+        for j in range(len(r_arr)):
+            z, r = z_grid[i, j], r_grid[i, j]
+            Bz_grid[i, j] = Bz(r, z, d=d, mu0=mu0, N=N, I=I, R=R)
+            Br_grid[i, j] = Br(r, z, d=d, mu0=mu0, N=N, I=I, R=R)
+
+
+    norm = numpy.sqrt(Br_grid**2 + Bz_grid**2)
+
+    pyplot.figure()
+    mesh = pyplot.pcolormesh(z_grid, r_grid, norm, shading="gouraud")
+    pyplot.pcolormesh(-z_grid, r_grid, norm, shading="gouraud")
+    pyplot.pcolormesh(z_grid, -r_grid, norm, shading="gouraud")
+    pyplot.pcolormesh(-z_grid, -r_grid, norm, shading="gouraud")
+    cbar = pyplot.colorbar(mesh)
+
+    pyplot.axvline(-d*0.5, ls="--", lw=2, color="black")
+    pyplot.axvline(d*0.5, ls="--", lw=2, color="black")
+    pyplot.axhline(-R, ls="--", lw=2, color="black")
+    pyplot.axhline(R, ls="--", lw=2, color="black")
+    pyplot.grid()
+    pyplot.legend(loc="best", ncol=2)
+    pyplot.xlabel(r"$z$", fontsize=30)
+    pyplot.ylabel(r"$y$", fontsize=30)
+    pyplot.tight_layout()
+    pyplot.savefig("Bz_vs_z_several_r.pdf")
+    # pyplot.show()
+    pyplot.close()
+
+def main():
+    # example1()
+    example2()
+    
 
 if __name__ == '__main__':
     main()
