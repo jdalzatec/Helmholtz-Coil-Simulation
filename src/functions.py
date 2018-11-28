8cfcc8c856ab361e7b8547821036aecce7c86899
dasabogals <dasabogals@unal.edu.co>
2018-11-28 16:57:41 -0500

    Added options to dispatch arguments.

--- src/functions.py
+++ src/functions.py
@@ -5,37 +5,37 @@ from functools import reduce
 import numpy
 
 
-@dispatch(list, Number, Number, Number)
+@dispatch((tuple, list, numpy.ndarray), Number, Number, Number)
 def Bz(coils, rho, z, mu0):
     return numpy.sum([mu0 * coil.Bz(rho, z) for coil in coils])
 
 
-@dispatch(list, Number, Number, Number)
+@dispatch((tuple, list, numpy.ndarray), Number, Number, Number)
 def Brho(coils, rho, z, mu0):
     return numpy.sum([mu0 * coil.Brho(rho, z) for coil in coils])
 
 
-@dispatch(list, Number, (list, numpy.ndarray), Number)
+@dispatch((tuple, list, numpy.ndarray), Number, (tuple, list, numpy.ndarray), Number)
 def Bz(coils, rho, z_arr, mu0):
     return numpy.array([Bz(coils, rho, z, mu0) for z in z_arr])
 
 
-@dispatch(list, Number, (list, numpy.ndarray), Number)
+@dispatch((tuple, list, numpy.ndarray), Number, (tuple, list, numpy.ndarray), Number)
 def Brho(coils, rho, z_arr, mu0):
     return numpy.array([Brho(coils, rho, z, mu0) for z in z_arr])
 
 
-@dispatch(list, (list, numpy.ndarray), Number, Number)
+@dispatch((tuple, list, numpy.ndarray), (tuple, list, numpy.ndarray), Number, Number)
 def Bz(coils, rho_arr, z, mu0):
     return numpy.array([Bz(coils, rho, z, mu0) for rho in rho_arr])
 
 
-@dispatch(list, (list, numpy.ndarray), Number, Number)
+@dispatch((tuple, list, numpy.ndarray), (tuple, list, numpy.ndarray), Number, Number)
 def Brho(coils, rho_arr, z, mu0):
     return numpy.array([Brho(coils, rho, z, mu0) for rho in rho_arr])
 
 
-@dispatch(list, (list, numpy.ndarray), (list, numpy.ndarray), Number)
+@dispatch((tuple, list, numpy.ndarray), (tuple, list, numpy.ndarray), (tuple, list, numpy.ndarray), Number)
 def Bz(coils, rho_arr, z_arr, mu0):
     Bz_grid = numpy.zeros(shape=(len(z_arr), len(rho_arr)))
     for i, z in enumerate(z_arr):
@@ -44,7 +44,7 @@ def Bz(coils, rho_arr, z_arr, mu0):
     return Bz_grid
 
 
-@dispatch(list, (list, numpy.ndarray), (list, numpy.ndarray), Number)
+@dispatch((tuple, list, numpy.ndarray), (tuple, list, numpy.ndarray), (tuple, list, numpy.ndarray), Number)
 def Brho(coils, rho_arr, z_arr, mu0):
     Brho_grid = numpy.zeros(shape=(len(z_arr), len(rho_arr)))
     for i, z in enumerate(z_arr):
@@ -53,7 +53,7 @@ def Brho(coils, rho_arr, z_arr, mu0):
     return Brho_grid
 
 
-@dispatch(list, (list, numpy.ndarray), (list, numpy.ndarray), Number)
+@dispatch((tuple, list, numpy.ndarray), (tuple, list, numpy.ndarray), (tuple, list, numpy.ndarray), Number)
 def Bz_Brho(coils, rho_arr, z_arr, mu0):
     Bz_grid = numpy.zeros(shape=(len(z_arr), len(rho_arr)))
     Brho_grid = numpy.zeros(shape=(len(z_arr), len(rho_arr)))
