dbeff3bbd5795d2758632920694ee51824f0b6fd
jdalzatec <jdalzatec@unal.edu.co>
2018-12-20 00:38:08 -0500

    Adding grid settings

---
+++ src/GridWindow.py
@@ -0,0 +1,99 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+from NumericEntry import NumericEntry
+
+class GridWindow(Gtk.Dialog):
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, "Grid", parent)
+
+
+        self.set_modal(True)
+        self.set_default_size(150, 100)
+
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+
+        # FUCKING LEFT BOX
+        boxLeft = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        label = Gtk.Label()
+        label.set_markup('<b>Z grid</b>')
+        
+        row1 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        lbl1 = Gtk.Label("Min. Z [m] =")
+        lbl1.set_halign(Gtk.Align.END)
+        self.txtMinZ = NumericEntry(float)
+        self.txtMinZ.set_width_chars(10)
+        row1.pack_start(lbl1, True, True, 0)
+        row1.pack_start(self.txtMinZ, False, False, 0)
+
+        row2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        lbl2 = Gtk.Label("Max. Z [m] =")
+        lbl2.set_halign(Gtk.Align.END)
+        self.txtMaxZ = NumericEntry(float)
+        self.txtMaxZ.set_width_chars(10)
+        row2.pack_start(lbl2, True, True, 0)
+        row2.pack_start(self.txtMaxZ, False, False, 0)
+        
+        row3 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        lbl3 = Gtk.Label("Points =")
+        lbl3.set_halign(Gtk.Align.END)
+        self.txtPointsZ = NumericEntry(int, "positive")
+        self.txtPointsZ.set_width_chars(10)
+        row3.pack_start(lbl3, True, True, 0)
+        row3.pack_start(self.txtPointsZ, False, False, 0)
+
+        boxLeft.pack_start(label, True, True, 0)
+        boxLeft.pack_start(row1, True, True, 0)
+        boxLeft.pack_start(row2, True, True, 0)
+        boxLeft.pack_start(row3, True, True, 0)
+
+
+
+
+        # FUCKING RIGHT BOX
+        boxRight = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        label = Gtk.Label()
+        label.set_markup('<b>ρ grid</b>')
+        
+        row1 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        lbl1 = Gtk.Label("Min. ρ [m] =")
+        lbl1.set_halign(Gtk.Align.END)
+        self.txtMinRho = NumericEntry(float, "positive")
+        self.txtMinRho.set_width_chars(10)
+        row1.pack_start(lbl1, True, True, 0)
+        row1.pack_start(self.txtMinRho, False, False, 0)
+
+        row2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        lbl2 = Gtk.Label("Max. ρ [m] =")
+        lbl2.set_halign(Gtk.Align.END)
+        self.txtMaxRho = NumericEntry(float, "positive")
+        self.txtMaxRho.set_width_chars(10)
+        row2.pack_start(lbl2, True, True, 0)
+        row2.pack_start(self.txtMaxRho, False, False, 0)
+        
+        row3 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        lbl3 = Gtk.Label("Points =")
+        lbl3.set_halign(Gtk.Align.END)
+        self.txtPointsRho = NumericEntry(int, "positive")
+        self.txtPointsRho.set_width_chars(10)
+        row3.pack_start(lbl3, True, True, 0)
+        row3.pack_start(self.txtPointsRho, False, False, 0)
+
+        boxRight.pack_start(label, True, True, 0)
+        boxRight.pack_start(row1, True, True, 0)
+        boxRight.pack_start(row2, True, True, 0)
+        boxRight.pack_start(row3, True, True, 0)
+
+        box.pack_start(boxLeft, True, True, 0)
+        box.pack_start(boxRight, True, True, 0)
+
+        content = self.get_content_area()
+        content.pack_start(box, False, False, 10)
+
+        self.add_button("Auto", Gtk.ResponseType.HELP)
+        self.add_button("Ok", Gtk.ResponseType.OK)
+        self.add_button("Cancel", Gtk.ResponseType.CANCEL)
+
+
+        self.show_all()
--- src/interface.py
+++ src/interface.py
@@ -3,6 +3,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
 
+from GridWindow import GridWindow
 from CoilsListBox import CoilsListBox
 from CoilListRow import CoilListRow
 from Presets import HelmholtzCoilPreset
@@ -19,9 +20,11 @@ class InputWindow():
         self.window = self.builder.get_object("wndInput")
         self.btnDeleteAll = self.builder.get_object("btnDeleteAll")
         self.btnAddCoil = self.builder.get_object("btnAddCoil")
-        self.btnSimulate = self.builder.get_object("btnSimulate")
         self.btnHelmholtzConfig = self.builder.get_object("btnHelmholtzConfig")
         self.scrListBox = self.builder.get_object("scrListBox")
+        self.btnSimulate = self.builder.get_object("btnSimulate")
+        self.btnSettings = self.builder.get_object("btnSettings")
+        self.lblGridInfo = self.builder.get_object("lblGridInfo")
         
         self.listBox = CoilsListBox()
         self.scrListBox.add_with_viewport(self.listBox)
@@ -31,33 +34,127 @@ class InputWindow():
         self.btnDeleteAll.connect("clicked", self.listBox.remove_all_coils)
         self.btnHelmholtzConfig.connect("activate", self.on_helmholtz_config)
         self.btnSimulate.connect("clicked", self.on_simulate)
+        self.btnSettings.connect("clicked", self.on_settings)
+
+
+        self.auto_grid = True
+        self.coils = []
+        self.z_min = 0.0
+        self.z_max = 0.0
+        self.z_points = 0
+        self.rho_min = 0.0
+        self.rho_max = 0.0
+        self.rho_points = 0
+
+        self.update_grid_info()
 
         self.window.show_all()
         Gtk.main()
     
+    def update_grid_info(self):
+        mode = "Auto" if self.auto_grid else "Manual"
+        text = "Grid (%s): Z from %s to %s (%i points); ρ from %s to %s (%i points);" % (
+            mode, self.z_min, self.z_max, self.z_points,
+            self.rho_min, self.rho_max, self.rho_points)
+        self.lblGridInfo.set_text(text)
     
     def on_helmholtz_config(self, widget):
         self.listBox.update(HelmholtzCoilPreset())
-    
+        self.collect_coils_values()
+        self.compute_grid_automatically()
+        self.update_grid_info()
 
-    def on_simulate(self, widget):
-        coils = []
+    def compute_grid_automatically(self):
+        if self.auto_grid:
+            if len(self.coils) > 0:
+                z_arr = [coil.pos_z for coil in self.coils]
+                self.z_min = min(z_arr)
+                self.z_max = max(z_arr)
+
+                radius_arr = [coil.radius for coil in self.coils]
+                self.rho_min = 0.0
+                self.rho_max = max(radius_arr)
+
+                if self.z_min == self.z_max:
+                    self.z_min = self.z_min - self.rho_max
+                    self.z_max = self.z_max + self.rho_max
+
+                self.z_points = 100
+                self.rho_points = 100
+            else:
+                self.z_min = 0.0
+                self.z_max = 0.0
+                self.z_points = 0
+                self.rho_min = 0.0
+                self.rho_max = 0.0
+                self.rho_points = 0
+
+
+    def collect_coils_values(self):
+        self.coils = []
         for row in list(self.listBox)[:-1]:
             coil_row, = row.get_children()
             if coil_row.validate_values():
                 coil = CreateCoil(**coil_row.get_values())
-                coils.append(coil)
+                self.coils.append(coil)
 
-        if len(coils) == 0:
-            raise Exception("Paila")
+        if len(self.coils) == 0:
+            # raise Exception("Paila")
+            print("paila")
+            return False
 
-        z_min = -0.5
-        z_max = 0.5
-        z_points = 100
-        rho_min = 0.0
-        rho_max = 1.0
-        rho_points = 100
-        self.simulation = Simulation(coils, z_min, z_max, z_points, rho_min, rho_max, rho_points)
+        return True
+
+
+    def on_simulate(self, widget):
+        ready = self.collect_coils_values()
+        if ready:
+            self.compute_grid_automatically()
+
+            self.simulation = Simulation(self.coils,
+                self.z_min, self.z_max, self.z_points,
+                self.rho_min, self.rho_max, self.rho_points)
+
+
+    def on_settings(self, widget):
+        ready = self.collect_coils_values()
+        if ready:
+            self.compute_grid_automatically()
+            self.auto_grid = False
+
+        dialog = GridWindow(self.window)
+
+        dialog.txtMinZ.set_property("text", str(self.z_min))
+        dialog.txtMaxZ.set_property("text", str(self.z_max))
+        dialog.txtPointsZ.set_property("text", str(self.z_points))
+        dialog.txtMinRho.set_property("text", str(self.rho_min))
+        dialog.txtMaxRho.set_property("text", str(self.rho_max))
+        dialog.txtPointsRho.set_property("text", str(self.rho_points))
+
+
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            self.z_min = float(dialog.txtMinZ.get_text())
+            self.z_max = float(dialog.txtMaxZ.get_text())
+            self.z_points = int(dialog.txtPointsZ.get_text())
+            self.rho_min = float(dialog.txtMinRho.get_text())
+            self.rho_max = float(dialog.txtMaxRho.get_text())
+            self.rho_points = int(dialog.txtPointsRho.get_text())
+        elif response == Gtk.ResponseType.HELP:
+            self.auto_grid = True
+            ready = self.collect_coils_values()
+            if ready:
+                self.compute_grid_automatically()
+        else:
+            pass
+
+        self.update_grid_info()
+
+        print(response)
+
+        dialog.destroy()
+        # Gtk.main()
 
 window = InputWindow("./interfaces/input2.glade")
 print("hola")
