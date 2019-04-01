eebb969df904bb918f7ce89a02efecac333f8f0c
jdalzatec <jdalzatec@unal.edu.co>
2019-04-01 00:01:02 -0500

    Solving the issues related with the links

--- src/About.py
+++ src/About.py
@@ -1,3 +1,19 @@
+import sys
+
+is_frozen = getattr(sys, 'frozen', False)
+frozen_temp_path = getattr(sys, '_MEIPASS', '')
+
+import os
+
+# This is needed to find resources when using pyinstaller
+if is_frozen:
+    basedir = frozen_temp_path
+else:
+    basedir = os.path.dirname(os.path.abspath(__file__))
+resource_dir = os.path.join(basedir, 'resources')
+
+
+
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -6,7 +22,7 @@ from gi.repository import Gtk
 class AboutWindow():
     def __init__(self, parent):
         self.builder = Gtk.Builder()
-        self.builder.add_from_file("interfaces/about.glade")
+        self.builder.add_from_file(resource_dir + "/about.glade")
 
         self.window = self.builder.get_object("wndAbout")
         self.btnClose = self.builder.get_object("btnClose")
